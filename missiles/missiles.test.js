27f46673e2f051432c9ef8fa523614a9ea3ae4c2
Vlad Hristoforov <vlad.hristoforov.jazzteam@gmail.com>
2021-09-08 17:53:22 +0300

    refactor launch func

---
+++ missiles/missiles.test.js
@@ -0,0 +1,11 @@
+
+
+// jest.useFakeTimers()
+
+// it('test', () => {
+//     const launchAll = require('./missiles')
+//     launchAll()
+
+//     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000)
+// })
+
